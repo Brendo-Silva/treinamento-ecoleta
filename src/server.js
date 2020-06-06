45bbbdb52992344c824bbda6ac28c34a05502068
Brendo Silva <breendo.01@gmail.com>
2020-06-06 08:57:33 -0300

    Criando servidor express e utilizando nunjuck no html

--- src/server.js
+++ src/server.js
@@ -1,4 +1,24 @@
 const express = require("express");
 const server = express();
+const nunjucks = require("nunjucks")
+
+server.use(express.static("public"))
+
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+server.get ("/", (req, res) => {
+    return res.render("index.html");
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search.html")
+})
 
 server.listen(3000);
