5c2e468a7d9f46adaf67c4069b13aa567a82b0ed
Brendo Silva <breendo.01@gmail.com>
2020-06-07 03:15:22 -0300

    Adicionando funcionalidades de busca e criação de modal

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ const db = require("./database/db")
 const nunjucks = require("nunjucks")
 
 server.use(express.static("public"))
+server.use(express.urlencoded({
+    extended: true
+}))
 
 nunjucks.configure("src/views", {
     express: server,
@@ -14,22 +17,60 @@ server.get ("/", (req, res) => {
     return res.render("index.html");
 })
 
-server.get("/create-point", (req, res) => {
-
-    console.log(req.query)
+server.get("/create-point", (req, res) => {   
 
     return res.render("create-point.html")
 })
 
 server.post("/savepoint", (req, res) => {
-    res.send("ok")
+
+    const query = `
+    INSERT INTO places (
+        image,
+        name,
+        address,
+        address2,
+        state,
+        city,
+        items
+    ) VALUES (?,?,?,?,?,?,?)
+`
+    const valuesArr = [
+       req.body.image,
+       req.body.name,
+       req.body.address,
+       req.body.address2,
+       req.body.state,
+       req.body.city,       
+       req.body.items,
+
+
+    ];
+
+    function afterInsertData(err) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no cadastro")
+        }       
+
+        res.render("create-point.html", { saved: true })
+    }
+
+    db.run(query, valuesArr, afterInsertData )    
 })
 
 
 
 server.get("/search", (req, res) => {
 
-    db.all(`SELECT * FROM places`, function (err, rows) {
+    const search = req.query.search
+    if(search == ""){
+        return res.render("search.html", { total: 0 })
+    }
+
+
+
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
         if (err) {
             return console.log(err)
         }
